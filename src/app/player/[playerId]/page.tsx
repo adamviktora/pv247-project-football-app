6262a6c969da5bf172a44ac9e4032e9cfeaf707e
Adam Viktora <xviktora@fi.muni.cz>
2023-12-03 23:35:38 +0100

    return button

--- src/app/player/[playerId]/page.tsx
+++ src/app/player/[playerId]/page.tsx
@@ -5,7 +5,7 @@ const PlayerDetail = ({ params }: { params: { playerId: string } }) => {
 
   return (
     <div className="flex w-full flex-col bg-slate-200">
-      <ReturnButton />
+      <ReturnButton standalone />
       <div className="h-full w-full bg-slate-300">
         TBD: Player with id {params.playerId}
       </div>
