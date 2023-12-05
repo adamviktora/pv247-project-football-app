291159fff34b9a8161df9620d62eb08bd2f4297f
Adam Viktora <xviktora@fi.muni.cz>
2023-12-05 09:37:28 +0100

    refactor: game detail

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -1,20 +1,7 @@
 import ReturnButton from "@/app/components/ReturnButton";
 import { getGameDetailById } from "@/server/game";
 import { formatDate } from "@/utils/date";
-import { Club } from "@prisma/client";
-import Link from "next/link";
-
-const clubCard = (club: Club, isHome?: boolean) => (
-  <Link
-    href={`/club?clubId=${club.id}`}
-    className={`flex ${
-      isHome ? "flex-row-reverse" : ""
-    } w-1/3 items-center gap-4 px-4 py-2 hover:cursor-pointer hover:rounded-lg hover:bg-gray-200`}
-  >
-    <img className="max-h-14" src={club.logoURL} alt="|Logo|" />
-    <div className="text-lg">{club.name}</div>
-  </Link>
-);
+import ClubCard from "./ClubCard";
 
 const GameDetail = async ({ params }: { params: { gameId: string } }) => {
   const game = await getGameDetailById(params.gameId);
@@ -28,11 +15,11 @@ const GameDetail = async ({ params }: { params: { gameId: string } }) => {
           <div>{formatDate(game.eventDate)}</div>
         </div>
         <div className="flex items-center gap-4">
-          {clubCard(game.homeClub, true)}
-          <div className="flex h-20 w-1/3 items-center justify-center rounded-lg bg-secondary-color text-5xl font-semibold">
+          <ClubCard isHome club={game.homeClub} />
+          <div className="flex h-20 w-1/3 items-center justify-center rounded-lg bg-secondary-color text-4xl font-semibold sm:text-5xl">
             {game.homeClubGoalCount} - {game.awayClubGoalCount}
           </div>
-          {clubCard(game.awayClub)}
+          <ClubCard club={game.awayClub} />
         </div>
       </div>
     </div>
