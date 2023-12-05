adedf4577fd92f39bb56fb317025d4dae535b09f
Adam Viktora <xviktora@fi.muni.cz>
2023-12-05 15:53:48 +0100

    remove forgotten unused endpoint

--- src/app/api/club/[id]/name/route.ts
+++
@@ -1,7 +0,0 @@
-import { getClubNameById } from "@/server/club";
-
-export async function GET({ params }: { params: { id: string } }) {
-  const clubName = await getClubNameById(params.id);
-
-  return Response.json(clubName);
-}
